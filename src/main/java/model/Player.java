82da310edc2fb5da86fca55059c6e69579cf31a5
Diego.Balaguer <didacb@gmail.com>
2025-06-21 19:01:04 +0200

    feat: add Element class and update dependents classes
    - Create Element base class with core variables
    - Refactor Certificate, Reward, Room to implement Element
    - Refactor Player for use Lombok and @ToString(exclude = "password")

--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -1,4 +1,23 @@
 package model;
 
+import lombok.Data;
+import lombok.ToString;
+
+import java.util.List;
+
+@Data
+@ToString(exclude = "password")
 public class Player {
+    private int id;
+    private String username;
+    private String email;
+    private String password;
+    private String phoneNumber;
+    private String address;
+    private String zipCode;
+    private String IdCity;
+    private boolean subscribed;
+    private List<Certificate> certificates;
+    private List<Reward> rewards;
+
 }
